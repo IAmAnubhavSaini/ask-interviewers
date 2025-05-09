fee5b573f8573910c12414c962693157c5404c79
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2025-05-09 13:24:35 +0530

    Update data copy

--- src/scripts/copy-data.sh
+++ src/scripts/copy-data.sh
@@ -5,24 +5,43 @@ set -euxo pipefail
 echo "[INFO] [START] Executing $0"
 
 DIR="../data/dist"
-DATA_DIR="../data/"
-CLI_DIR="../cli/"
-LIB_DIR="../lib/"
-BLOG_DIR="../blog/"
+DATA_DIR="../data"
+CLI_DIR="../cli"
+LIB_DIR="../lib"
+BLOG_DIR="../blog"
+SERVER_DIR="../server/src"
 
 if [ ! -d ${DIR} ]; then
     echo "[INFO] Directory ${DIR} does not exist."
     echo "[INFO] Building data project."
-    cd ${DATA_DIR} && npm run build && cd ../scripts
+    cd ${DATA_DIR} && npm install && npm run build && cd ../scripts
     if [ ! -d ${DIR} ]; then
         echo "[INFO] Directory ${DIR} still does not exist. Aborting."
         exit 1
     fi
-else
-    echo "[INFO] Directory ${DIR} exist. Copying."
-    cp -r ${DIR} ${CLI_DIR}
-    cp -r ${DIR} ${LIB_DIR}
-    cp -r ${DIR} ${BLOG_DIR}
 fi
+
+if [ ! -d ${CLI_DIR} ]; then
+    echo "[INFO] Creating directory ${CLI_DIR}."
+    mkdir -p ${CLI_DIR}
+fi
+if [ ! -d ${LIB_DIR} ]; then
+    echo "[INFO] Creating directory ${LIB_DIR}."
+    mkdir -p ${LIB_DIR}
+fi
+if [ ! -d ${BLOG_DIR} ]; then
+    echo "[INFO] Creating directory ${BLOG_DIR}."
+    mkdir -p ${BLOG_DIR}
+fi
+if [ ! -d ${SERVER_DIR} ]; then
+    echo "[INFO] Creating directory ${SERVER_DIR}."
+    mkdir -p ${SERVER_DIR}
+fi
+
+echo "[INFO] Directory ${DIR} exist. Copying."
+cp -r "${DIR}" "${CLI_DIR}/data/"
+cp -r "${DIR}" "${LIB_DIR}/data/"
+cp -r "${DIR}" "${BLOG_DIR}/data/"
+cp -r "${DIR}" "${SERVER_DIR}/data/"
 
 echo "[INFO] [DONE] Data successfully copied."
